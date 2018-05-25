3d21639e3af2cdd5a8ba128e013574a557fbb48f
Elżbieta Kukułka <ellakaras@gmail.com>
2018-05-25 21:52:03 +0200

    Add Redux DevTools

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CommentsListContainers from './CommentsListContainer';
+import DevTools from './DevTools';
 
 const App = () => {
   return (
     <div className="App">
      <h2>Add comment</h2>
       <CommentsListContainers />
+      <DevTools />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import DevTools from './DevTools';
 
 import reducer from './reducer';
 
 import {addComment} from './actions';
 
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  DevTools.instrument()
+);
 
 ReactDOM.render(
   <Provider store={store}>
